bb30a2c7749496ce7a9782f08f608da971eebc96
Mammoon8705 <mamoonkhan663@gmail.com>
2026-07-26 12:13:47 +0000

    update neoma

--- AI/neoma.js
+++ AI/neoma.js
@@ -248,21 +248,7 @@ function initializeNeoma() {
 
             });
 
-            const response = await fetch("/api/chat", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    message: lastMessage
-                })
-            });
-
-            const text = await response.text();
-
-            console.log(text);
-
-            const data = JSON.parse(text);
+            const data = await response.json();
 
             addBotMessage(data.reply);
 
